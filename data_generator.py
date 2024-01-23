84f43fe1de055f0f6725109f50264158b94f8389
elizabethm26 <102920310+elizabethm26@users.noreply.github.com>
2024-01-23 18:51:48 +0200

    Update data_generator.py

--- data_generator.py
+++ data_generator.py
@@ -3,12 +3,13 @@ import pandas as pd
 def create_sample_data():
     
     credit_products_data = {
-        'product_id': [1, 2],
-        'product_name': ['Personal Loan', 'Credit Card'],
-        'product_description': ['Quick personal loan...', 'Rewards credit card...'],
-        'interest_rate': [8.5, 18.9],
-        'loan_term': [24, None],  # None може да означава неопределен срок
-        'max_credit_limit': [20000, 5000]
+        'product_id': [1, 2, 3, 4, 5, 6],
+        'product_name': ['Personal Loan', 'Credit Card', 'Student Loan', 'Mortgage', 'Auto Loan', 'Business Loan'],
+        'product_description': ['Quick personal loan...', 'Rewards credit card...', 'Cover cost of education…',
+                                'Finance the purchase of real estate', 'Purchase vehicles', 'For business needs'],
+        'interest_rate': [8.5, 18.9, 3.73, 3.45, 5.99, 7.62],
+        'loan_term': [24, None, 120, None, 60, 36],  # None може да означава неопределен срок
+        'max_credit_limit': [20000, 5000, 10000, 800000, 200000, 500000]
     }
 
     credit_products = pd.DataFrame(credit_products_data)
@@ -18,12 +19,12 @@ def create_sample_data():
 
     
     user_profiles_data = {
-        'user_id': [101, 102],
-        'user_name': ['John', 'Jane'],
-        'income': [60000, 80000],
-        'expenses': [40000, 50000],
-        'credit_history': ['Good', 'Excellent'],
-        'preferred_product': ['Personal Loan', 'Credit Card']
+        'user_id': [101, 102, 103, 104, 105, 106],
+        'user_name': ['John', 'Jane', 'Misho', 'Mimi', 'Vanko', 'Preslava'],
+        'income': [60000, 80000, 25000, 50000, 28000, 500000],
+        'expenses': [40000, 50000, 18000, 30000, 20000, 300000],
+        'credit_history': ['Good', 'Excellent', 'Average', 'Good', 'Average', 'Excellent'],
+        'preferred_product': ['Personal Loan', 'Credit Card', 'Student Loan', 'Mortgage', 'Auto Loan', 'Business Loan']
     }
 
     user_profiles = pd.DataFrame(user_profiles_data)
